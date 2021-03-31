62fd33567a48847801337f96b2be56769fe287d1
aliwan937 <aliwan937@gmail.com>
2021-03-30 20:31:15 -0700

    alice name added (#3)
    Co-authored-by: dmstudent1 <dmstudent1@dmstudent1s-MacBook-Air.local>

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,8 +2,10 @@ function Home() {
   return (
     <main className="container">
       <h1>Home</h1>
+      
       <p>
         <h2>Team members:</h2> 
+        <p>Alice Wang</p>
         <p>Serena Chen</p>
       </p>
     </main>    
