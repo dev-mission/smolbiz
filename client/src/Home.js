0765c5670dc96e41953195ce962eaf2e52c9d3fb
yelhsams <51773182+yelhsams@users.noreply.github.com>
2021-05-14 12:11:05 -0500

    [WIP] Header and Home Bootstrap Design (#26)
    * Added a pic of a cat :)
    
    * updated front Home page
    
    * added Shop CRUD, updated models to be singular
    
    * renamed ItemsForm to ItemForm for clairity
    
    * fixed shop bug in shop form to erase and replace Item
    
    * Add username to User model, update create-admin script accordingly. Create Shop/User foreign key reference, set UserId automatically on create.
    
    * Allow any logged in User to create a Shop for now
    
    * Associate Item with Shop and User, set UserId in API route, add Shop dropdown list to Item form
    
    * Make Items an AuthProtectedRoute
    
    * Make sure changes in dropdown are set on object
    
    * Add rest of the associations
    
    * Edited Badges, Shoppers attributes to camel case
    
    * Added username in registeration
    
    * Added Kudos, Shopper, Shoutouts and Badges to App.js Path
    
    * updated Register.js, added a redirection based on business owner or shopper profile
    
    * Made changes to Register.js according to Francis comments
    
    * Header.js Dropdown fixed
    
    * Register.js conflict resolved, Home.js started creating rows for content
    
    * Added banner, search bar, and carousel to Home.js with basic bootstrap elements
    
    Co-authored-by: Francis Li <mail@francisli.com>

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,66 @@
 function Home() {
   return (
     <main className="container">
+      <div className="row">
+        <div className="col d-flex justify-content-center">
+          <h5>Connect and Shop in Your Neighborhood</h5>
+        </div>
+      </div>
+      <div className="row">
+        {/*search bar JUSTIFY CENTER*/}
+        <form class="col-md-6 offset-3">
+          <div class="row">
+            <div className="offset-2 col-6">
+              <input class="form-control" type="search" placeholder="What can I help you find today?" aria-label="Search" />
+            </div>
+            <div className="col-4">
+             <button class="btn btn-outline-success" type="submit">Search</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="row">
+        {/*carousel of photos and captions*/}
+        <div id="carouselExampleCaptions" class="carousel slide col-8 offset-2" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://www.tripsavvy.com/thmb/TXvFG9C3Gdxou7yY-UgZAIn416g=/1920x1280/filters:no_upscale():max_bytes(150000):strip_icc()/DSC05433-5bdc80c4c9e77c00510fbe98.jpg" class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="https://www.tripsavvy.com/thmb/TXvFG9C3Gdxou7yY-UgZAIn416g=/1920x1280/filters:no_upscale():max_bytes(150000):strip_icc()/DSC05433-5bdc80c4c9e77c00510fbe98.jpg" class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="https://www.tripsavvy.com/thmb/TXvFG9C3Gdxou7yY-UgZAIn416g=/1920x1280/filters:no_upscale():max_bytes(150000):strip_icc()/DSC05433-5bdc80c4c9e77c00510fbe98.jpg" class="d-block w-100" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+
+      </div>
       <h1>Home</h1>
       <p>
         <h2>Team members:</h2> 
@@ -15,7 +75,7 @@ function Home() {
       </p>
       <img src = "https://memegenerator.net/img/instances/73979818.jpg"></img>
       <p>indeed very smol</p>
-    </main>    
+    </main>  
   );
 }
 export default Home;
